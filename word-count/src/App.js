c89c4f7c82141929ede8272de7958193f52ea7df
DejaLFlynn <dejaflynn@pursuit.org>
2021-06-22 18:31:04 -0400

    rearranged files

--- word-count/src/App.js
+++ word-count/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Input from './Components/Input';
+import WordCount from './Components/WordCount';
 function App() {
   return (
     <div className="App">
       Word Counter
-     <Input/>
+     <WordCount/>
     </div>
   );
 }
