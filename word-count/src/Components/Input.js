cb13224362e69762ea63f5a9bf72f55ec943a03f
DejaLFlynn <dejaflynn@pursuit.org>
2021-06-22 18:28:20 -0400

    working on logic for sentence and paragraph

--- word-count/src/Components/Input.js
+++ word-count/src/Components/Input.js
@@ -1,25 +1,23 @@
 import React, { useState, useContext, useEffect } from "react";
-
+import { useInput } from "./WordCount";
 
 export default function Input() {
-  const [text, setText] = useState("");
+  //   const [text, setText] = useState("");
+  const text = useInput("");
   const [charCount, setCharCount] = useState(0);
-//   const [wordCount, setWordCount] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
   const [sentenceCount, setSentenceCount] = useState(0);
   const [paragraphCount, setParagraphCount] = useState(0);
-
-
 
   const isWord = (str) => {
     var alphaNumericFound = false;
     for (var i = 0; i < str.length; i++) {
       var code = str.charCodeAt(i);
       if (
-        (code > 47 && code < 58) || // numeric (0-9)
-        (code > 64 && code < 91) || // upper alpha (A-Z)
+        (code > 47 && code < 58) ||
+        (code > 64 && code < 91) ||
         (code > 96 && code < 123)
       ) {
-        // lower alpha (a-z)
         alphaNumericFound = true;
         return alphaNumericFound;
       }
@@ -27,39 +25,90 @@ export default function Input() {
     return alphaNumericFound;
   };
 
-  //word counter
-  const wordCounter = (words) => {
-    var words = text.value.split(" ");
-    var wordCount = 0;
-    for (var i = 0; i < words.length; i++) {
-      if (words[i] !== " " && isWord(words[i])) {
-        wordCount++;
+  const handleWordCount = () => {
+    let input = text.toString();
+    let newText = input.split("  ");
+    let count = 0;
+    for (let i = 0; i < newText.length; i++) {
+      if (newText[i] !== " " && isWord(newText[i])) {
+        count++;
       }
     }
-    words.value = wordCount;
+    setWordCount(count);
   };
 
+  const handleSentenceCount = () => {
+    let input = text.toString();
+    let newText = input.split(".");
+    let count = 0;
+    for (let i = 0; i < newText.length; i++) {
+      if (newText[i] !== " " && isWord(newText[i])) {
+        count++;
+      }
+    }
+    setSentenceCount(count);
+  };
 
+  const handleParagraphCount = () => {
+    let input = text.toString();
+    let newText = input.split(/\r?\n|\r/);
+    let count = 0;
+    for (let i = 0; i < newText.length; i++) {
+      if (newText[i] !== " " && isWord(newText[i])) {
+        count++;
+      }
+    }
+    setParagraphCount(count);
+  };
 
+  //   const handleWordCount=() =>{
+  //     // console.log(typeof(text))
+  //     let input = text.toString();
+  //     console.log(typeof(input))
+  //     input = input.replace(/(^\s*)|(\s*$)/gi,"");
+  //     input = input.replace(/[ ]{2,}/gi," ");
+  //     input = input.replace(/\n /,"\n");
+  //     let newCount=  input.split('  ').length;
+  //     // console.log(input.replace(/\n /,"\n"))
+  //      setWordCount(newCount)
+
+  //  }
+
+  useEffect(() => {
+    handleWordCount();
+    handleSentenceCount();
+    handleParagraphCount();
+  }, []);
 
 
   return (
     <div>
-
-       <textarea
+      <textarea
         type="text"
         rows={5}
         className="full_height_Width"
-        onChange={e => setCharCount(e.target.value.length)}
+        onChange={(e) => setCharCount(e.target.value.length)}
       />
-      <p>  <strong>Character Count:</strong> {charCount}</p>
-      <br/>
-      <p>  <strong>Word Count:</strong> {wordCounter}</p>
-      <br/>
-      <p>  <strong>Sentence Count:</strong> {sentenceCount}</p>
-      <br/>
-      <p>  <strong>Paragraph Count:</strong> {paragraphCount}</p>
-      <br/>
+      <p>
+        {" "}
+        <strong>Character Count:</strong> {charCount}
+      </p>
+      <br />
+      <p>
+        {" "}
+        <strong>Word Count:</strong> {wordCount}
+      </p>
+      <br />
+      <p>
+        {" "}
+        <strong>Sentence Count:</strong> {sentenceCount}
+      </p>
+      <br />
+      <p>
+        {" "}
+        <strong>Paragraph Count:</strong> {paragraphCount}
+      </p>
+      <br />
     </div>
   );
 }
