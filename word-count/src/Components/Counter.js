1f9c4a60476b94edac33476d24fd8d6d01e74b88
DejaLFlynn <dejaflynn@pursuit.org>
2021-06-23 09:39:39 -0400

    working on additional features

--- word-count/src/Components/Counter.js
+++ word-count/src/Components/Counter.js
@@ -6,56 +6,20 @@ export default function Counter() {
   const [wordCount, setWordCount] = useState(0);
   const [sentenceCount, setSentenceCount] = useState(0);
   const [paragraphCount, setParagraphCount] = useState(0);
+  const [bigramCount, setBigramCount] = useState(0);
 
-  const removeBreaks = (arr) => {
-    const index = arr.findIndex((el) => el.match(/\r?\n|\r/g));
-
-    if (index === -1) return arr;
-
-    const newArray = [
-      ...arr.slice(0, index),
-      ...arr[index].split(/\r?\n|\r/),
-      ...arr.slice(index + 1, arr.length),
-    ];
-
-    removeBreaks(newArray);
-  };
-  const removeEmptyElements = (arr) => {
-    const index = arr.findIndex((el) => el.trim() === "");
-
-    if (index === -1) return arr;
-
-    arr.splice(index, 1);
-
-    return removeEmptyElements(arr);
-  };
-
-  const setCounts = (value) => {
+  const setAllCounts = (value) => {
     const trimmedValue = value.trim();
-    const words = (trimmedValue.split(" "));
-    const sentences = (trimmedValue.split("."));
-    const paragraphs = (trimmedValue.split(/\r?\n|\r/));
-    // var findOcurrences = function(text, first, second) {
-    
-    //     var result = [];
-    //     text = text.split(" ");
-    
-    //     while(text.length !== 0){
-    //         // If the text first two elements are first and second, the third element is not undefined
-    //         if(text[0] === first && text[1] === second && text[2] !== undefined){
-    //                          The third element // push result
-    //             result.push(text[2]);
-    //                          // Remove the first two elements
-    //             text.shift();
-    //             text.shift();
-    //         }else{
-    //                          // otherwise removes the first element
-    //             text.shift();
-    //         }
-    //     }
-    
-    //     return result;
-    // };
+    const words = trimmedValue.split(" ");
+    const sentences = trimmedValue.split(".");
+    const paragraphs = trimmedValue.split(/\r?\n|\r/);
+    const countUniqueWords = () => {
+        const words = new Set();
+        let value = text
+        console.log(text)
+        value.toLowerCase().replace(/\w+/g, word => words.add(word));
+        return words.size;        
+    }
     
 
 
@@ -64,10 +28,13 @@ export default function Counter() {
     setWordCount(value === "" ? 0 : words.length);
     setSentenceCount(value === "" ? 0 : sentences.length);
     setParagraphCount(value === "" ? 0 : paragraphs.length);
+    setBigramCount(countUniqueWords)
+    
+ 
 
   };
 
-  const handleChange = (e) => setCounts(e.target.value);
+  const handleChange = (e) => setAllCounts(e.target.value);
   return (
     <div>
       <textarea rows="15" onChange={handleChange} value={text}></textarea>
@@ -79,6 +46,8 @@ export default function Counter() {
         <strong>Sentence Count:</strong> {sentenceCount}
         <br />
         <strong>Paragraph Count:</strong> {paragraphCount}
+        <br />
+        <strong>Bigram Count:</strong> {bigramCount}
       </p>
     </div>
   );
