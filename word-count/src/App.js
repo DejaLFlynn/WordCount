aae2ed9bdf1d84d0a7fc31bcf40181eaac563a22
DejaLFlynn <dejaflynn@pursuit.org>
2021-06-23 11:33:50 -0400

    added darkmode toggle

--- word-count/src/App.js
+++ word-count/src/App.js
@@ -1,13 +1,24 @@
-// import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Counter from "./Components/Counter";
 
-import './App.css';
-
-import Counter from './Components/Counter';
 function App() {
+  const storedDarkMode = localStorage.getItem("DARK_MODE");
+  const [darkMode, setDarkMode] = useState(storedDarkMode);
+
+  const toggleDarkMode = () => setDarkMode(darkMode ? false : true);
+
+  useEffect(() => {
+    localStorage.setItem("DARK_MODE", darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="App">
+    <div className="App" data-theme={darkMode ? "dark" : "light"}>
       Word Counter
-     <Counter/>
+      <Counter />
+      <button onClick={toggleDarkMode}>
+        {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      </button>
     </div>
   );
 }
